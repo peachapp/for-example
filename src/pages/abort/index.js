a293e85296a5528514b7faa6719827631f4e69ba
peachapp <571596435@qq.com>
2021-12-07 11:04:04 +0800

    page

---
+++ src/pages/abort/index.js
@@ -0,0 +1,7 @@
+import './index.css';
+
+const Abort = () => {
+  return <div>关于</div>
+};
+
+export default React.memo(Abort);
---
+++ src/pages/home/index.js
@@ -0,0 +1,7 @@
+import './index.css';
+
+const Home = () => {
+  return <div>homehome</div>
+};
+
+export default React.memo(Home);
